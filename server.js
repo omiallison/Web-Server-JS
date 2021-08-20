d266528a2b211d312102d6a35c3e1d35e5551d9d
Omi Allison <70861457+omiallison@users.noreply.github.com>
2021-08-20 10:22:34 -0400

    Update server.js
    Changed 'port' to prop port. 
    
    Note: You will need to change it to your desired port.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const fs = require('fs')
-const port = 8547
+const port = 0000
 
 const server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' })
